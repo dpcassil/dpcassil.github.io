db7ecfb4d650b52b880ec375ce4cd634690a6bde
me@danielcassil.com <me@danielcassil.com>
2015-09-19 14:01:18 -0500

    save

--- js/houser.js
+++ js/houser.js
@@ -15200,15 +15200,9 @@ HOUSER.define('js/ajax',[], function () {
 					url = url.substring(0, url.length - 1); // Remove last &
 				}
 
-				$.ajax({
-					type: 'GET',
-					url: this.proxy.dev,
-					data: {url: url},
-					contentType: 'text/xml'
-				}).done(function (resp) {
+				$.get(this.proxy.live, {url: url}).done(function (resp) {
 					deferred.resolve(resp);
 				});
-
 				return deferred;
 			},
 			post: function (data, service, callback) {
